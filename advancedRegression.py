8c186eda18e4763d8edc53f75734fa429927e5f0
Prathik612 <prathik612@gmail.com>
2023-10-17 17:55:29 +0530

    final

--- advancedRegression.py
+++ advancedRegression.py
@@ -7,21 +7,12 @@ import seaborn as sb
 import numpy as np
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, TensorDataset
-from pathlib import Path
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using: {device}\n")
 
 train_data = pd.read_csv("house-prices-advanced-regression-techniques\\train.csv")
 
-#print(train_data.head)
-
-#print(train_data.isnull().sum().to_string())
-#print(train_data.shape)
-
-#plt.figure(figsize = (8,6))
-#sb.heatmap(train_data.isnull(), cbar=False , cmap = 'magma')
-#plt.show()
 
 def cleanData(data = train_data):
     data = data.drop(columns=["Id","Alley", "PoolQC", "Fence", "MiscFeature", "SalePrice"], errors='ignore', axis=1)
@@ -31,7 +22,6 @@ def cleanData(data = train_data):
 
     data_cat = pd.get_dummies(data[cat_cols], dummy_na=True)
     data_num = data[num_cols].apply(lambda x: (x - x.mean()) / (x.std()))
-    #data_num = data_num.fillna(0)
     mean = data_num.mean(axis=0)
     data_num = data_num.fillna(mean)
 
@@ -42,28 +32,25 @@ def cleanData(data = train_data):
 y_data = train_data["SalePrice"].copy()
 X_data = cleanData()
 y_data = np.log(y_data, where=y_data != 0)
-#print(X_data)
-print(X_data.iloc[0].to_string())
-print(X_data.shape)
-
-
-#print(y_data.shape)
-
-#X_data = torch.tensor(X_data.values).float()
-#y_data = torch.tensor(y_data.values).float().unsqueeze(dim=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, train_size=0.8, random_state=42)
 
-#print(X_train.shape)
-#print(y_train.shape)
 
 X_train = torch.tensor(X_train.values).float().to(device)
 y_train = torch.tensor(y_train.values).float().unsqueeze(dim=1).to(device)
 X_test = torch.tensor(X_test.values).float().to(device)
 y_test = torch.tensor(y_test.values).float().unsqueeze(dim=1).to(device)
 
+X_data_temp = X_data.copy()
+X_data = torch.tensor(X_data.values).float().to(device)
+y_data = torch.tensor(y_data.values).float().unsqueeze(dim=1).to(device)
+
 BATCH_SIZE = 32
 torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+X_dataloader = TensorDataset(X_data,y_data)
+X_dataloader = DataLoader(X_dataloader, batch_size=BATCH_SIZE, shuffle=False)
 
 dataset_train = TensorDataset(X_train, y_train)
 dataset_test = TensorDataset(X_test, y_test)
@@ -80,29 +67,17 @@ class LinearRegressionv0(nn.Module):
                                                    out_features=hidden_units),
                                          nn.Linear(in_features=hidden_units,
                                                    out_features= output_shape),
-                                         nn.ReLU())
-    def forward(self, x):
-        return self.layer_stack(x)
-
-class LinearRegressionv1(nn.Module):
-    def __init__(self, input_shape:int, hidden_units: int, output_shape: int):
-        super().__init__()
-        self.layer_stack = nn.Sequential(nn.Linear(in_features= input_shape,
-                                                   out_features= hidden_units),
-                                         nn.ReLU(),
-                                         nn.Linear(in_features= hidden_units,
-                                                   out_features=hidden_units),
-                                         nn.ReLU(),
-                                         nn.Linear(in_features=hidden_units,
-                                                   out_features= output_shape),)
+                                         nn.RReLU())
     def forward(self, x):
         return self.layer_stack(x)
     
-class LinearRegressionv2(nn.Module):
+class LinearRegressionv1(nn.Module):
     def __init__(self, input_shape:int, hidden_units: int, output_shape: int):
-        super().__init__()
-        self.layer_stack = nn.Sequential(nn.Linear(in_features= input_shape,
+      super().__init__()
+      self.layer_stack = nn.Sequential(nn.Linear(in_features= input_shape,
                                                    out_features= hidden_units),
+                                         nn.Linear(in_features= hidden_units,
+                                                   out_features=hidden_units),
                                          nn.Linear(in_features= hidden_units,
                                                    out_features=hidden_units),
                                          nn.Linear(in_features= hidden_units,
@@ -111,13 +86,17 @@ class LinearRegressionv2(nn.Module):
                                                    out_features= output_shape),
                                          nn.ReLU())
     def forward(self, x):
-        return self.layer_stack(x)
+      return self.layer_stack(x)
     
-model = LinearRegressionv1(input_shape=314, hidden_units=211, output_shape=1).to(device)
+model = LinearRegressionv0(input_shape=314, hidden_units=211, output_shape=1).to(device)
 
 loss_fn = nn.MSELoss()
 optimizer = optim.SGD(params=model.parameters(), lr=0.001)
+#optimizer = optim.SGD(params=model.parameters(), lr=0.001 , nesterov=True, dampening=0, momentum=0.5)
 epochs = 300
+
+#model= LinearRegressionv0(input_shape=314, hidden_units=471, output_shape=1).to(device)
+optimizer = optim.SGD(params=model.parameters(), lr=0.003)  #SGD optimizer
 
 def train_step(model: nn.Module,
                data_loader: torch.utils.data.DataLoader,
@@ -176,12 +155,16 @@ def eval_model(model: torch.nn.Module,
         loss /= len(data_loader)
         loss = f"{loss:.3f}"
 
-    return {"model_name": model.__class__.__name__,
-            "model_loss": loss}
+    y = torch.exp(y)
+    y_pred = torch.exp(y_pred)
+    errors = (y_pred - y).flatten().detach().cpu().numpy()
+    mae = np.abs(errors).mean()
+    rmse = np.sqrt(((errors)**2).mean())
 
-training_loss = []
-testing_loss = []
-epoch_count = []
+    return {"model_name": model.__class__.__name__,
+            "model_loss": loss,
+            "MAE": mae,
+            "RMSE": rmse}
 
 for epoch in range(epochs):
     train_loss = train_step(data_loader=train_dataloader, 
@@ -192,11 +175,6 @@ for epoch in range(epochs):
         model=model,
         loss_fn=loss_fn)
     
-    if epoch%10 == 0:
-        epoch_count.append(epoch)
-        training_loss.append(train_loss.detach().cpu().numpy())
-        testing_loss.append(test_loss.detach().cpu().numpy())
-    
     if epoch%50 == 0:
         print(f"Epoch: {epoch}\n---------")
         print(f"Train loss: {train_loss:.5f}")
@@ -204,8 +182,10 @@ for epoch in range(epochs):
 
 
 model_results = eval_model(model=model,
-                             data_loader=test_dataloader,
+                             data_loader=X_dataloader,
                              loss_fn=loss_fn)
+
+print(model_results)
 
 
 test_data = pd.read_csv("house-prices-advanced-regression-techniques\\test.csv")
@@ -239,8 +219,3 @@ y_sub = y_sub.cpu().detach().numpy() # Change to numpy
 submission = pd.DataFrame({"Id":sub_id, "SalePrice":y_sub}) # Make dataframe for submissino
 submission.to_csv('submission.csv', index=False)
 
-
-#print("New: \n")
-#print(train_data.isnull().sum().to_string())
-#print(X_data.shape)
-#print(y_data.shape)
